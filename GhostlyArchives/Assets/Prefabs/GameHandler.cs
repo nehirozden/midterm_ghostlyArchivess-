f6023db2df25e8594873f328297bbd62c8eb65ac
Nehir Ozden <nehirlaraozden@gmail.com>
2024-10-08 14:44:29 -0400

    connected start and end scene, added esc option
    Started working on making the health background stuff, and the end scene needs to be linked after health stuff is added

--- GhostlyArchives/Assets/Prefabs/GameHandler.cs
+++ GhostlyArchives/Assets/Prefabs/GameHandler.cs
@@ -6,9 +6,16 @@ using UnityEngine.SceneManagement;
 
 public class GameHandler : MonoBehaviour {
 
+    // Method to start the game (load the start scene)
+    public void StartScene(){
+        // As in, when the start button is clicked, this scene is loaded
+        SceneManager.LoadScene("KevinScene"); // Change this to your start scene name
+    }
+
     // Method to restart the game
     public void RestartGame(){
-        SceneManager.LoadScene("NehirScene"); // Change this to the scene you want to restart
+        // As in, when the restart button is clicked, this scene is loaded
+        SceneManager.LoadScene("KevinScene"); // Change this to the scene you want to restart
     }
 
     // Method to quit the game
@@ -18,5 +25,13 @@ public class GameHandler : MonoBehaviour {
         #else
         Application.Quit();
         #endif
+    }
+
+    // Update is called once per frame
+    void Update(){
+        // Checks if the Escape key is pressed
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            QuitGame(); // Call the QuitGame function when Escape is pressed
+        }
     }
 }
