a0b3131cd54283915bd4839b79158b2d9951ff81
Nehir Ozden <nehirlaraozden@gmail.com>
2024-10-08 21:09:47 -0400

    added health

--- GhostlyArchives/Assets/Prefabs/GameHandler.cs
+++ GhostlyArchives/Assets/Prefabs/GameHandler.cs
@@ -6,10 +6,16 @@ using UnityEngine.SceneManagement;
 
 public class GameHandler : MonoBehaviour {
 
+    public GameObject healthText;
+    public PlayerController currHealth;
+
     // Method to start the game (load the start scene)
     public void StartScene(){
         // As in, when the start button is clicked, this scene is loaded
         SceneManager.LoadScene("KevinScene"); // Change this to your start scene name
+        Debug.Log("in start");
+        updateStatsDisplay();
+        Debug.Log("after display");
     }
 
     // Method to restart the game
@@ -29,9 +35,23 @@ public class GameHandler : MonoBehaviour {
 
     // Update is called once per frame
     void Update(){
+        updateStatsDisplay();
         // Checks if the Escape key is pressed
         if (Input.GetKeyDown(KeyCode.Escape)){
             QuitGame(); // Call the QuitGame function when Escape is pressed
         }
     }
+
+    public void updateStatsDisplay(){
+        Debug.Log("here");
+        Text healthTextTemp = healthText.GetComponent<Text>();
+        // currHealth = GetComponent<PlayerController>();
+        Debug.Log("after");
+
+
+        healthTextTemp.text = "HEALTH: " + currHealth.health;
+        Debug.Log(currHealth.health);
+    }
+
+
 }
